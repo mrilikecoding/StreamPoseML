be8628cb16974fb109497570c405db6dcd282e41
mrilikecoding <nathanrgreen@gmail.com>
2023-07-11 16:26:34 -0700

    add debug flag and some diagnostic output

--- start.sh
+++ start.sh
@@ -14,6 +14,22 @@ then
     exit 1
 fi
 
+# Check if required ports are available
+for port in 3000 5001; do
+    if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null ; then
+        echo "Port $port is already in use, please free up this port and try again."
+        exit 1
+    else
+        echo "Port $port is available."
+    fi
+done
+
+# If debug mode is on, output Docker and Docker Compose versions
+if [[ $1 == "--debug" ]]; then
+    echo "Docker version: $(docker --version)"
+    echo "Docker Compose version: $(docker-compose --version)"
+fi
+
 # Log into Docker Hub
 echo "Please enter your Docker Hub credentials:"
 docker login
@@ -23,7 +39,39 @@ docker pull mrilikecoding/pose_parser_api:latest
 docker pull mrilikecoding/web_ui:latest
 
 # Start the services with Docker Compose
-docker-compose up -d && sleep 15 &
+echo "Starting services with Docker Compose..."
+DOCKER_COMPOSE_OUTPUT=$(docker-compose up -d 2>&1) || {
+    echo "Docker Compose exited with an error. Output was:"
+    echo $DOCKER_COMPOSE_OUTPUT
+    exit 1
+}
+
+sleep 15
+
+# If debug mode is on, output Docker system info, Docker stats, Docker disk usage and Docker Compose logs
+if [[ $1 == "--debug" ]]; then
+    echo "Docker System Info:"
+    docker system info
+
+    echo "Docker Disk Usage:"
+    docker system df
+
+    echo "Host Disk Usage:"
+    df -h
+
+    echo "Docker stats (for 15 seconds):"
+    docker stats --no-stream --format "table {{.Container}},{{.CPUPerc}},{{.MemUsage}}" & 
+    STATS_PID=$!
+    sleep 15
+    if kill -0 $STATS_PID 2>/dev/null; then
+        kill $STATS_PID
+    else
+        echo "Docker stats process already completed."
+    fi
+
+    echo "Docker Compose logs:"
+    docker-compose logs
+fi
 
 # Wait for the services to start
 echo "Waiting for the services to start..."
