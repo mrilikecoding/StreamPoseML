7591b6e6af033710c05a4f26aba8297cb1f62661
Nathan Green <nathanrgreen@gmail.com>
2023-11-20 13:36:44 -0800

    improve build setup

--- setup.py
+++ setup.py
@@ -1,36 +1,15 @@
 from setuptools import setup, find_packages
+import os
 
 def read_requirements():
-    with open('./stream_pose_ml/requirements.txt') as req:
+    setup_py_dir = os.path.dirname(os.path.realpath(__file__))
+    requirements_path = os.path.join(setup_py_dir, 'stream_pose_ml', 'requirements.txt')
+    with open(requirements_path) as req:
         return req.read().splitlines()
 
 setup(
-    name='stream_pose_ml',
-    version='0.1.0',  # Update the version number for new releases
-    author='Nate Green',
-    author_email='nate@nate.green',
-    description='A toolkit for realtime video classification tasks.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/mrilikecoding/StreamPoseML',
-    project_urls={
-        "Bug Tracker": "https://github.com/mrilikecoding/StreamPoseML/issues"
-    },
-    license='MIT', 
+    install_requires=read_requirements(),
     package_dir={'': 'stream_pose_ml'},
     packages=find_packages(where='stream_pose_ml'),
-    install_requires=read_requirements(),
-    classifiers=[
-        'Development Status :: 3 - Alpha',  # Update to '5 - Production/Stable' if applicable
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: MIT License',  # Or your chosen license
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    python_requires='>=3.10',
 )
 
