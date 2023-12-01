27d1ca89cfec92bdcc7722bf1d151d118c4b45b2
Nathan Green <nathanrgreen@gmail.com>
2023-12-01 10:46:15 -0600

    format black

--- stream_pose_ml/stream_pose_ml/learning/dataset.py
+++ stream_pose_ml/stream_pose_ml/learning/dataset.py
@@ -1,7 +1,13 @@
 class Dataset:
-    """ All data for a dataset as well as a segmented representation of the data. """
-    def __init__(self, all_frames: list = [], labeled_frames: list = [], unlabeled_frames: list = []) -> None:
-        """ Init a Dataset object.
+    """All data for a dataset as well as a segmented representation of the data."""
+
+    def __init__(
+        self,
+        all_frames: list = [],
+        labeled_frames: list = [],
+        unlabeled_frames: list = [],
+    ) -> None:
+        """Init a Dataset object.
 
 
         Args:
@@ -16,5 +22,3 @@ class Dataset:
         self.labeled_frames = labeled_frames
         self.unlabeled_frames = unlabeled_frames
         self.segmented_data = None
-    
-    
