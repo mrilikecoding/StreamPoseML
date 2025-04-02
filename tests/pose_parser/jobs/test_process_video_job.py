dfd1bd927242cc7bacfaa0aef3c1b63b197271b6
Nathan Green <nathanrgreen@gmail.com>
2025-04-01 20:19:00 -0700

    restructure test

--- tests/pose_parser/jobs/test_process_video_job.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from stream_pose_ml.jobs.process_video_job import ProcessVideoJob
-
-class TestProcessVideoJob(unittest.TestCase):
-    def test_process_video(self):
-        pass
