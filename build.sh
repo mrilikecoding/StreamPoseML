32ba73e4e41ffefa35cad6e0c8eaca8e3bf669c2
Nathan Green <nathanrgreen@gmail.com>
2025-06-17 18:27:39 -0700

    update build script

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ docker buildx use multiplat_builder
 
 # Building stream_pose_ml_api
 echo "Building stream_pose_ml_api for multiple platforms..."
-docker buildx build --platform linux/amd64,linux/arm64 -t mrilikecoding/stream_pose_ml_api:latest --push ./stream_pose_ml
+docker buildx build --platform linux/amd64,linux/arm64 -t mrilikecoding/stream_pose_ml_api:latest --push ./api
 
 # Building web_ui
 echo "Building web_ui for multiple platforms..."
-docker buildx build --platform linux/amd64,linux/arm64 -t mrilikecoding/web_ui:latest --push ./web_ui
+docker buildx build --platform linux/amd64,linux/arm64 -t mrilikecoding/stream_pose_ml_web_ui:latest --push ./web_ui
+
+# Building mlflow
+echo "Building mlflow for multiple platforms..."
+docker buildx build --platform linux/amd64,linux/arm64 -t mrilikecoding/stream_pose_ml_mlflow:latest --push ./mlflow
 
 echo "All images have been built and pushed successfully!"
