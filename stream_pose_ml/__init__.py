8adb0c78cf988bf89e2067772c7b384c806fab5b
Nathan Green <nathanrgreen@gmail.com>
2025-06-17 17:59:53 -0700

    update ci again

--- stream_pose_ml/__init__.py
+++ stream_pose_ml/__init__.py
@@ -7,6 +7,7 @@ import sys
 import os
 from pathlib import Path
 
+
 # Make top-level components accessible directly from the module
 from stream_pose_ml.stream_pose_client import StreamPoseClient
 from .ml_flow_client import MLFlowClient
